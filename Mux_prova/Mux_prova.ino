608cc0e443b2e24291ee56d3154711e11a1a9da5
smartsolutionpz <123073855+smartsolutionpz@users.noreply.github.com>
2024-02-08 02:17:13 +0100

    Update Mux_prova.ino

--- Mux_prova/Mux_prova.ino
+++ Mux_prova/Mux_prova.ino
@@ -1,101 +1,55 @@
-//Pin di controllo Mux 
-int s0 =  15 ; 
-int s1 =  16 ; 
-int s2 =  17 ; 
-int s3 =  18 ;
+const int SPin[4] = {3, 4, 5, 6}; // 4 Pin usati per inviare il codice binario
+const int EPin = 2; /* Pin Enable
+                      - se impostato HIGH interrotto fisicamente il collegamento tra il pin SIG e quello Yxx scelto
+                      - se impostato LOW viene stabilito il collegamento tra il pin SIG e quello Yxx scelto */
 
-int y0 =  2 ; 
-int y1 =  3 ; 
-int y2 =  4 ; 
-int y3 =  5 ;
-int y4 =  6 ; 
-int y5 =  7 ; 
-int y6 =  8 ; 
-int y7 =  9 ;
+const int SIG = 9; // SIG pin
 
-//Mux in "SIG" pin 
-int SIG_pin =  19 ;
-//Mux in "EN" pin 
-int EN_pin =  14 ;
+const int STable[16][4] = {
+  // Crea un Array con i valori binari da richiamare in base al canale Y scelto
+  // s0, s1, s2, s3, canale
+  {0,  0,  0,  0}, // Y0
+  {1,  0,  0,  0}, // Y1
+  {0,  1,  0,  0}, // Y2
+  {1,  1,  0,  0}, // Y3
+  {0,  0,  1,  0}, // Y4
+  {1,  0,  1,  0}, // Y5
+  {0,  1,  1,  0}, // Y6
+  {1,  1,  1,  0}, // Y7
+  {0,  0,  0,  1}, // Y8
+  {1,  0,  0,  1}, // Y9
+  {0,  1,  0,  1}, // Y10
+  {1,  1,  0,  1}, // Y11
+  {0,  0,  1,  1}, // Y12
+  {1,  0,  1,  1}, // Y13
+  {0,  1,  1,  1}, // Y14
+  {1,  1,  1,  1}  // Y15
+};
 
-
-void setup( ) { 
-  pinMode( s0 ,  OUTPUT ) ;  
-  pinMode( s1 ,  OUTPUT ) ;  
-  pinMode( s2 ,  OUTPUT ) ;  
-  pinMode( s3 ,  OUTPUT ) ; 
-  pinMode( SIG_pin ,  OUTPUT ) ;
-  pinMode( EN_pin ,  OUTPUT ) ;
-
-  digitalWrite( s0 ,  LOW ) ; 
-  digitalWrite( s1 ,  LOW ) ; 
-  digitalWrite( s2 ,  LOW ) ; 
-  digitalWrite( s3 ,  LOW ) ;
-  digitalWrite( SIG_pin ,  HIGH ) ;
-  digitalWrite( EN_pin ,  LOW ) ;
-
-  pinMode( y0 ,  INPUT_PULLUP ) ;  
-  pinMode( y1 ,  INPUT_PULLUP ) ;  
-  pinMode( y2 ,  INPUT_PULLUP ) ;  
-  pinMode( y3 ,  INPUT_PULLUP ) ; 
-  pinMode( y4 ,  INPUT_PULLUP ) ;  
-  pinMode( y5 ,  INPUT_PULLUP ) ;  
-  pinMode( y6 ,  INPUT_PULLUP ) ;  
-  pinMode( y7 ,  INPUT_PULLUP ) ;
-
-
-
-  Serial.begin(9600) ; 
-
-
-
-
+void YSelect(int Y){
+  digitalWrite(SPin[0], STable[Y][0]);
+  digitalWrite(SPin[1], STable[Y][1]);
+  digitalWrite(SPin[2], STable[Y][2]);
+  digitalWrite(SPin[3], STable[Y][3]);
 }
 
+void setup() {
+  pinMode(EPin, OUTPUT);// Inizializza EPin come OUTPUT
+  digitalWrite(EPin, LOW); // presetta EPin LOW, leggere la nota all'inizio del programma per i dettagli
+  
+  pinMode(SIG, OUTPUT); // Inizializza SIG come OUTPUT
+  digitalWrite(SIG, HIGH); // Presetta SIG HIGH, serve per il nostro esperimeto poi andrà settato a seconda delle necessità
 
-void loop( ) {
-
-  //Esegui il loop e leggi tutti i 16 valori 
-  //Riporta il valore sul canale 6 è: 346 
-  for ( int i =  2 ; i <  10 ; i ++ ) { 
-    Serial.print (digitalRead(i)) ; 
-    Serial.print (" ") ;    
+  for (int i = 0; i < 4; i++)
+  {
+    pinMode(SPin[i], OUTPUT); // Inizializza tutti gli Spin come OUTPUT
+    digitalWrite(SPin[i], LOW); // Setta tutti gli Spin LOW
   }
-  Serial.println() ;
-
 }
 
-
-int readMux ( int canale ) { 
-  int controlPin [ ]  =  { s0 , s1 , s2 , s3 } ;
-
-  int muxChannel [ 16 ] [ 4 ] = { 
-    { 0 , 0 , 0 , 0 } ,  //canale 0 
-    { 1 , 0 , 0 , 0 } ,  //canale 1 
-    { 0 , 1 , 0 , 0 } ,  // canale 2 
-    { 1 , 1 , 0 , 0 } ,  // canale 3 
-    { 0 , 0 , 1 , 0 } ,  // canale 4 
-    { 1 , 0 , 1 , 0 } ,  // canale 5 
-    { 0 , 1 , 1 , 0 } ,  //canale 6 
-    { 1 , 1 , 1 , 0 } ,  //canale 7 
-    { 0 , 0 , 0 , 1 } ,  //canale 8 
-    { 1 , 0 , 0 , 1 } ,  //canale 9 
-    { 0 , 1 , 0 , 1 } ,  //canale 10 
-    { 1 , 1 , 0 , 1 } ,  //canale 11 
-    { 0 , 0 , 1 , 1 } ,  //canale 12 
-    { 1 , 0 , 1 , 1 } ,  //canale 13 
-    { 0 , 1 , 1 , 1 } ,  //canale 14 
-    { 1 , 1 , 1 , 1 }   //canale 15 
-  } ;
-
-  // esegue un ciclo attraverso i 4 sig 
-  for ( int i =  0 ; i <  4 ; i ++ ) { 
-    digitalWrite ( controlPin [ i ] , muxChannel [ canale ] [ i ] ) ; 
+void loop() {
+  for (int i=0; i<16; i++) {
+    YSelect(i); // modificare il valore da 0 a 15 per cegliere quale pin Y selezionare
+    //delay(50);
   }
-
-  //legge il valore sul pin SIG 
-  int val =  analogRead ( SIG_pin ) ;
-
-  //restituisce il valore 
-  return val ; 
 }
